35ef06213f2c5b6a5f904cec0b99e3f2a27dea19
Wiwie Sanjaya <wiwiewei18@gmail.com>
2025-12-04 15:58:55 +0900

    fix: db connect using host, post, user, password, name

--- src/infra/drizzle/db.ts
+++ src/infra/drizzle/db.ts
@@ -5,7 +5,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export const db = drizzle(pool);
